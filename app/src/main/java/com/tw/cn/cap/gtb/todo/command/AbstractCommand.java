be965836a42f31aa177dd8dae3486ea67149df89
Menghan Bai <majesty2@foxmail.com>
2022-10-30 16:12:49 +0800

    refactor: AbstractCommand

--- app/src/main/java/com/tw/cn/cap/gtb/todo/command/AbstractCommand.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/command/AbstractCommand.java
@@ -4,20 +4,20 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public abstract class AbstractCommand implements Command{
+public class AbstractCommand implements Command{
 
     @Override
-    public String getCommand(String[] args) {
+    public String getCommand(String... args) {
         return args[0];
     }
 
     @Override
-    public List<String> getArgs(String[] args) {
+    public List<String> getArgs(String... args) {
         return new ArrayList<>(Arrays.asList(args).subList(1, args.length));
     }
 
     @Override
-    public void execute() {
-
+    public List<String> execute() {
+        return null;
     }
 }
