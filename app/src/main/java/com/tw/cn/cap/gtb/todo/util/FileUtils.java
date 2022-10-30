bfd318da8457ab7f1cb5c56b3be82629317d094f
Menghan Bai <majesty2@foxmail.com>
2022-10-30 14:54:01 +0800

    add: writeFile util

--- app/src/main/java/com/tw/cn/cap/gtb/todo/util/FileUtils.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/util/FileUtils.java
@@ -1,25 +1,41 @@
 package com.tw.cn.cap.gtb.todo.util;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class FileUtils {
+    static final String path = System.getProperty("user.home") + File.separator + ".todo" + File.separator + "tasks";
+    static final File file = new File(path);
+
     public static List<String> readFile() {
-        String path = System.getProperty("user.home") + File.separator + ".todo" + File.separator + "tasks";
-        File file = new File(path);
         try {
-            Scanner scanner = new Scanner(new FileInputStream(file));
+            FileInputStream fileInputStream = new FileInputStream(file);
+            Scanner scanner = new Scanner(fileInputStream);
             List<String> result = new ArrayList<>();
             while (scanner.hasNext()) {
                 result.add(scanner.nextLine());
             }
             return result;
         } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void writeFile(List<String> args) {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(file);) {
+            List<String> fileContentPrev = new ArrayList<>(readFile());
+            StringBuilder fileContentNow = new StringBuilder();
+            for (String s : fileContentPrev) {
+                fileContentNow.append(s).append("\n");
+            }
+            for (String arg : args) {
+                fileContentNow.append(arg).append("\n");
+            }
+            fileOutputStream.write(fileContentNow.toString().getBytes(StandardCharsets.UTF_8));
+            fileOutputStream.flush();
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
