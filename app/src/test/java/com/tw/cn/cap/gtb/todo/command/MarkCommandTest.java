b4f788470319d1908d727dd2cda8d1d85d3f1a4d
Menghan Bai <majesty2@foxmail.com>
2022-10-30 19:34:23 +0800

    add: MarkCommand test

---
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/command/MarkCommandTest.java
@@ -0,0 +1,19 @@
+package com.tw.cn.cap.gtb.todo.command;
+
+import com.tw.cn.cap.gtb.todo.util.FileUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MarkCommandTest extends CommandUtilTest {
+    @Test
+    void listMark() {
+        MarkCommand markCommand = new MarkCommand();
+        markCommand.listMark("mark", "1", "3");
+        List<String> readFile = FileUtils.readFile();
+        Assertions.assertEquals(List.of("# To be done", "1 bar", "# completed", "1 foo", "2 Task 00", "3 foo bar"), new ListCommand().listSortAndShow(readFile));
+    }
+}
