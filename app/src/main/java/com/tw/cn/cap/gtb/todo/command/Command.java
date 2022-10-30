d0d1ff1592b381b63dbffff91fa35ac748e14ce2
Menghan Bai <majesty2@foxmail.com>
2022-10-30 15:26:12 +0800

    add: command interface

---
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/command/Command.java
@@ -0,0 +1,11 @@
+package com.tw.cn.cap.gtb.todo.command;
+
+import java.util.List;
+
+public interface Command{
+    String getCommand(String[] args);
+
+    List<String> getArgs(String[] args);
+
+    void execute();
+}
