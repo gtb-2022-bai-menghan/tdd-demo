11c8a04edbacc0f3c121c4c6a6739e7f1996552c
Menghan Bai <majesty2@foxmail.com>
2022-10-30 17:15:02 +0800

    add: system auto delete some import

--- app/src/test/java/com/tw/cn/cap/gtb/todo/command/ListCommandTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/command/ListCommandTest.java
@@ -4,17 +4,14 @@ import com.tw.cn.cap.gtb.todo.util.FileUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ListCommandTest {
     ListCommand listCommand = new ListCommand();
     @Test
     void listSort() {
         List<String> readFile = FileUtils.readFile();
-        List<String> listSort = listCommand.listSort(readFile);
+        List<String> listSort = listCommand.listSortAndShow(readFile);
         Assertions.assertEquals(List.of("# To be done","1 foo", "2 bar", "3 Task 00", "# completed","1 foo bar"), listSort);
     }
 
