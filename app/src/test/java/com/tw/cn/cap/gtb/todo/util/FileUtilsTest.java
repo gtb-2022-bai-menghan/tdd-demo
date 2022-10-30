1cc406b37226fdfcfb0961605afef07224500eeb
Menghan Bai <majesty2@foxmail.com>
2022-10-30 14:54:08 +0800

    add: writeFile util test

--- app/src/test/java/com/tw/cn/cap/gtb/todo/util/FileUtilsTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/util/FileUtilsTest.java
@@ -14,4 +14,14 @@ class FileUtilsTest {
         List<String> fileContent = FileUtils.readFile();
         Assertions.assertEquals(List.of("foo", "bar"), fileContent);
     }
+
+    @Test
+    void writeFile() {
+        List<String> fileContentPrev = FileUtils.readFile();
+        List<String> addFileContent = new ArrayList<>(fileContentPrev);
+        addFileContent.add("Task 00");
+        FileUtils.writeFile(addFileContent);
+        List<String> fileContentNow = FileUtils.readFile();
+        Assertions.assertEquals(addFileContent, fileContentNow);
+    }
 }
